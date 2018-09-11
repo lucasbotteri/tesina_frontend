0d730ecab7af4cd7ce795b86ec9fce853478898d
Lucas Botteri <lucasbotteri@hotmail.com>
2018-09-11 18:33:15 -0300

    Fixed import statement

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/views/Home.vue';
-import SymbolList from '@/components/symbol/SymbolList';
+import SymbolList from '@/components/symbol/SymbolList.vue';
 
 Vue.use(Router);
 
