011b5e7cc2964193320739d7f0201e76a84c6c86
Lucas Botteri <lucasbotteri@hotmail.com>
2018-09-11 18:28:38 -0300

    Fix not checking undefined

--- src/api/Axios.ts
+++ src/api/Axios.ts
@@ -1,10 +1,10 @@
-import {AxiosInstance} from 'axios';
+import { AxiosInstance } from 'axios';
 import Axios from 'axios';
 const BASE_URL = 'http://localhost:3030/';
 export default class API {
 
     public static getInstance(): AxiosInstance {
-        if (this.client === null) {
+        if (this.client === undefined) {
             this.client = Axios.create({
                 baseURL: BASE_URL,
                 withCredentials: false,
