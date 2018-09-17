e6b622d226e22a8fe260318249de2a16a8fe6422
Lucas Botteri <lucasbotteri@hotmail.com>
2018-09-17 00:00:50 -0300

    Addded login template component and router with guard

---
+++ src/components/authentication/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-flex xs4 class="grey lighten-4">
+    <v-container style="position: relative;top: 13%;" class="text-xs-center">
+      <v-card flat>
+        <v-card-title primary-title>
+          <h4>Login</h4>
+        </v-card-title>
+        <v-form>
+        <v-text-field prepend-icon="person" name="Username" label="Username"></v-text-field>
+        <v-text-field prepend-icon="lock" name="Password" label="Password" type="password"></v-text-field>
+        <v-card-actions>
+          <v-btn primary large block>Login</v-btn>
+        </v-card-actions>
+        </v-form>
+      </v-card>
+    </v-container>
+  </v-flex>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Inject } from "vue-property-decorator";
+import {account} from '@/store/Account';
+
+@Component({
+  name: "Login"
+})
+export default class LoginVue extends Vue {
+ async created() {
+   await account.login({username: 'email@hotmail.com', password: '1234'})
+ }
+}
+</script>
+
+
+
--- src/router.ts
+++ src/router.ts
@@ -2,10 +2,15 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/views/Home.vue';
 import SymbolList from '@/components/symbol/SymbolList.vue';
+import Login from '@/components/authentication/Login.vue';
+import { getToken } from '@/helper/authentication';
+
+const PUBLIC_PAGES = ['/login'];
 
 Vue.use(Router);
 
-export default new Router({
+
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -16,8 +21,26 @@ export default new Router({
     },
     {
       path: '/simbolos',
-      name : 'symbols',
+      name: 'symbols',
       component: SymbolList,
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    { path: '*', redirect: '/' },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  const authRequired = !PUBLIC_PAGES.includes(to.path);
+  const loggedIn = getToken();
+  if (authRequired && !loggedIn) {
+    return next('/login');
+  }
+
+  next();
+});
+
+export default router;
