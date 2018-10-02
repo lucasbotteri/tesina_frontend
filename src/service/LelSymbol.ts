6590d53c3f93aac48ac533d2ade287335e0174dd
Lucas Botteri <lucasbotteri@hotmail.com>
2018-10-01 22:55:00 -0300

    exporting as module and not class

--- src/service/LelSymbol.ts
+++ src/service/LelSymbol.ts
@@ -1,16 +1,15 @@
 import Api from '@/api/Axios';
 import Service from '@/interface/Service';
 import LelSymbol from '@/model/LelSymbol';
-import { injectable } from 'inversify';
 
 
-
-@injectable()
-export default class SymbolService implements Service<LelSymbol> {
-
-    public async list(): Promise<LelSymbol[]> {
-        const response = await Api.getInstance().get('/symbol/');
-        const data = response.data.data;
-        return data.map((d: any) => new LelSymbol(d.name, d.type));
-    }
+export async function list(): Promise<LelSymbol[]> {
+    const response = await Api.getInstance().get('/symbol/');
+    const data = response.data.data;
+    return data.map((d: any) => new LelSymbol(d.name, d.type));
 }
+
+
+export default {
+    list,
+};
