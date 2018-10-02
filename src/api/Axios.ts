410caa37f596f1a84cc2f551b41537a46cf35a57
Lucas Botteri <lucasbotteri@hotmail.com>
2018-10-01 22:54:11 -0300

    Added authorization interceptor to axios

--- src/api/Axios.ts
+++ src/api/Axios.ts
@@ -1,5 +1,6 @@
 import { AxiosInstance } from 'axios';
 import Axios from 'axios';
+import { isLoggedIn, getToken, removeToken } from '@/helper/authentication';
 const BASE_URL = 'http://localhost:3030/';
 export default class API {
 
@@ -13,6 +14,18 @@ export default class API {
                     'Content-Type': 'application/json',
                 },
             });
+            this.client.interceptors.response.use((response) => {
+                return response;
+            }, (error) => {
+                if (error.response.data.code === 401 && error.response.data.name === 'TokenExpiredError') {
+                    removeToken();
+                } else {
+                    return Promise.reject(error);
+                }
+            });
+        }
+        if (isLoggedIn()) {
+            this.client.defaults.headers.Authorization = getToken();
         }
         return this.client;
     }
