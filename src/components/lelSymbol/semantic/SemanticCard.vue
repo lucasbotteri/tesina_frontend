41415a31fc989b6a2ed974e6a824f98bfc5a8983
Lucas Botteri <lucasbotteri@hotmail.com>
2019-01-01 22:12:02 -0300

    Added hidden scroll

--- src/components/lelSymbol/semantic/SemanticCard.vue
+++ src/components/lelSymbol/semantic/SemanticCard.vue
@@ -6,13 +6,13 @@
     <v-layout align-center justify-space-between row>
         <v-list-tile-content>
             <v-list-tile-title>{{this.semantic.description}}</v-list-tile-title>
-            <v-list-tile-sub-title>
+            <v-list-tile-sub-title class="hiddenScroll">
               <v-btn round color="grey lighten-2" class="caption" depressed small
                 v-for="symbolReferenced in this.semantic.symbolsReferenced"
                 :key="symbolReferenced.id"
                 :to="{ name: 'symbol-detail', params:{ id: symbolReferenced.id } }"
               >{{symbolReferenced.name}}</v-btn>
-              </v-list-tile-sub-title>
+            </v-list-tile-sub-title>
         </v-list-tile-content>
         <v-btn @click="removeSemantic()" icon>
             <v-icon color="red">delete</v-icon>
@@ -51,3 +51,14 @@ export default class SemanticCardVue extends Vue {
 
 
 
+<style >
+  .hiddenScroll {
+    display: flex;
+    flex-wrap: nowrap;
+    overflow-x: auto;
+  }
+
+  .hiddenScroll::-webkit-scrollbar {
+    display: none;
+  }
+</style>
